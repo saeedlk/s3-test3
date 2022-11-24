e75415db326374189dc665a4a1bc0b5b55fbd336
Saeid <98920239+saeedlk@users.noreply.github.com>
2022-11-24 17:58:12 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,9 @@ provider "aws" {
 
 resource "aws_s3_bucket" "b" {
   bucket = "tttstt"
-  bucket_prefix = var.bucket_prefix
   acl = var.acl
   
  #  versioning {
  #   enabled = var.versioning
  # }
-  
-  tags = var.tags
 }
