df5acb2078c0cbce59eda90adcaac08b60d3bc95
Saeid <98920239+saeedlk@users.noreply.github.com>
2022-11-24 17:42:59 +0330

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,26 @@
 variable "aws_region" {
-  description = "The AWS region to use to create resources."
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "bucket_prefix" {
     type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
-    default     = "my-s3bucket-"
+    default     = "my-s3-bucket-lk-5"
 }
 
-variable "tags" {
-    type        = map
-    description = "(Optional) A mapping of tags to assign to the bucket."
-    default     = {
-        environment = "DEV"
-        terraform   = "true"
-    }
-}
+# variable "tags" {
+#     type        = map
+#     description = "(Optional) A mapping of tags to assign to the bucket."
+#     default     = {
+#         environment = "DEV"
+#         terraform   = "true"
+#     }
+# }
 
-variable "versioning" {
-    type        = bool
-    description = "(Optional) A state of versioning."
-    default     = true
-}
+# variable "versioning" {
+#     type        = bool
+#     description = "(Optional) A state of versioning."
+#     default     = true
+# }
 
 variable "acl" {
     type        = string
